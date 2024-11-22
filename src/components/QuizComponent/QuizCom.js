9619d4a3d9930c4c2206204177aa7746b69fcf0e
rajat dev <rajatdev971@gmail.com>
2024-11-23 03:44:09 +0530

    add Quiz folder

---
+++ src/components/QuizComponent/QuizCom.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const QuizCom = () => {
+  return (
+    <div className='container'>
+  <h1 className='heading'>quiz</h1>
+  <hr/>
+  <h2>what is your name</h2>
+  <ul>
+    <li>Rajat</li>
+    <li>Ritika</li>
+    <li>harshit</li>
+    <li>sanjil</li>
+  </ul>
+  <button>Next</button>
+  <div className='index'>1 of 5 question</div>
+    </div>
+  )
+}
+
+export default QuizCom
